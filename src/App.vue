a5ac3e27f3c2739bbfd614f6dda2632322f54f6f
Sai Prasad Malkapuram <Saiprasad.Malkapuram@tanla.com>
2023-02-21 00:34:40 +0530

    modified app vuew

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+    <ul>
+      <li v-for="song in getSongs()" :key="song.id" @click="newestAudio()">
+        {{song.name}}
+        </li>
+    </ul>
+    <button @click="newestAudio()"> play</button>
+</div>
+
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// import a from "/assets/music/"
 
 export default {
   name: 'App',
+  data(){
+    return {songsList:[
+      {
+        id:1,
+        name:"DJ Tillu",
+        path:"'@/music/2.mp3'"
+      },
+      { id:2,
+        name:"Prematho",
+        path:"@/music/1.mp3"
+      }
+    ]
+    }
+  },
   components: {
-    HelloWorld
+  },
+  methods:{
+        newestAudio() {
+          // return "https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"
+          // return '@/Prematho.mp3'
+          // let p = ''
+          let sound = ( new Audio( require('@/music/2.mp3') )).play()
+          sound.play()
+      },
+      getSongs(){
+        return this.songsList
+      }
+
   }
 }
 </script>
