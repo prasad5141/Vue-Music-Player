2bdc4e651fa685829eed5a5a7312da2bc7757c21
Sai prasad <prasad.51415@gmail.com>
2023-02-26 23:41:56 +0530

    play, pause, previous and next feature implemented

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
 <div>
     <ul>
-      <li v-for="song in getSongs()" :key="song.id" @click="newestAudio()">
+      <li v-for="(song, id) in getSongs()" :key="id" @click="playFromList(id)">
         {{song.name}}
         </li>
     </ul>
-    <button @click="newestAudio()"> play</button>
+    <button @click="playPrevious()">
+      Previous
+    </button>
+    <button @click="palyPauseAction(1)">
+      <div v-if="isPlayerRunning">
+        Pause
+      </div>
+      <div v-else>
+       play
+       </div>
+    </button>
+    
+    <button @click="playNext()">
+      Next
+    </button>
 </div>
 
 
@@ -19,33 +33,85 @@
 export default {
   name: 'App',
   data(){
-    return {songsList:[
-      {
-        id:1,
-        name:"DJ Tillu",
-        path:"'@/music/2.mp3'"
-      },
-      { id:2,
-        name:"Prematho",
-        path:"@/music/1.mp3"
+    return {
+      audio:null,
+      isPlayerRunning:false,
+      currentSongID:null,
+      songsList:{
+        1:{
+            name:"Prematho",
+            path:"https://raw.githubusercontent.com/prasad5141/Vue-Music-Player/main/src/music/1.mp3"
+          },
+        2:{
+            name:"DJ Tillu",
+            path:"https://raw.githubusercontent.com/prasad5141/Vue-Music-Player/main/src/music/2.mp3"
+          },
+        3:{
+          name:"Love Debba",
+          path:"https://raw.githubusercontent.com/prasad5141/Vue-Music-Player/main/src/music/3.mp3"
+        },
+
       }
-    ]
+      
+    
     }
   },
   components: {
   },
   methods:{
-        newestAudio() {
-          // return "https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"
-          // return '@/Prematho.mp3'
-          // let p = ''
-          let sound = ( new Audio( require('@/music/2.mp3') )).play()
-          sound.play()
+        palyPauseAction(songId) {
+          if (this.currentSongID === null){
+            this.currentSongID = parseInt(songId)
+            let path = "https://raw.githubusercontent.com/prasad5141/Vue-Music-Player/main/src/music/"+songId+".mp3"
+            this.audio.src = path;
+          }else{
+            let path = "https://raw.githubusercontent.com/prasad5141/Vue-Music-Player/main/src/music/"+this.currentSongID+".mp3"
+            this.audio.src = path;
+          }          
+          if (!this.isPlayerRunning){
+            this.audio.play()
+            this.isPlayerRunning = true;
+          }else{
+            this.audio.pause()
+            this.isPlayerRunning = false;
+          }
       },
       getSongs(){
         return this.songsList
+      },
+      playFromList(songId){
+        this.currentSongID = parseInt(songId)
+        let path = "https://raw.githubusercontent.com/prasad5141/Vue-Music-Player/main/src/music/"+songId+".mp3"
+        console.log(path)
+        this.audio.src = path;
+        this.audio.play()
+        this.isPlayerRunning = true;
+        
+      },
+      playPrevious(){
+        let songID = this.currentSongID-1
+        let songIdList = Object.keys(this.songsList);
+        console.log(songIdList,songID.toString())
+        if (songIdList.includes(songID.toString())){
+          this.playFromList(songID)
+        }else{
+          console.log(songID)
+        }
+      },
+      playNext(){
+        let songID = this.currentSongID+1
+        let songIdList = Object.keys(this.songsList);
+        console.log(songIdList,songID.toString())
+        if (songIdList.includes(songID.toString())){
+          this.playFromList(songID)
+        }else{
+          console.log(songID)
+        }
       }
 
+  },
+  created(){
+     this.audio = new Audio(  )
   }
 }
 </script>
